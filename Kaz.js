f9d41904d85267b7fa2e56a834e3c541ef39f842
zagcha <64097681+zagcha@users.noreply.github.com>
2020-10-10 14:17:47 +0300

    Create Kaz.js

---
+++ Kaz.js
@@ -0,0 +1,61 @@
+$("<div class='fr borderg' style='padding:2px;background-color:white;margin-right:4px;'>تصميم وبرمجه: <a href='http://www.dookmobile.com/'>دوك موبايل</a></div>").insertAfter($(".loginstat"));
+}, 1000);
+
+"https:" != location.protocol && (location.protocol = "https:"), $(`<img style="width:100%;    margin-top: 0px;pointer-events:none;margin-bottom: 1px;" src="https://b.top4top.io/p_17399q80r1.jpg" ><div id="asim13"><a id="asim21" href="contact.html" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">اتصل بنا</a><a id="asim21" href="mobile.html" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">التطبيق</a><a id="asim21" href="rules.html" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">القوانين</a><a id="asim21" href="Shortcut.html" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">اختصارات</a><a id="asim22" href="sub.html" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">اشتراكات</a><a id="asim22" href="style.html" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">الابراج</a><a id="asim22" href="ruless.html" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">ق.السوابر</a><a id="asim22" href="radio.html" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">راديو العراق</a></div></div><center>`).insertBefore(".nav-tabs"), $(".nav-tabs").addClass("fl").css("width", "100%"), $(".tc span[class=\"fa fl\"]").addClass("fa-check");
+var nopm = !0,
+    nonot = !0;
+$(".hand.nosel.fl.uzr.border").css({
+    margin: "1px 0",
+    borderRadius: "0px"
+}), $(`<div><marquee direction="right" id="as43"scrollamount="3">آهلا وسهلا بكم في شات <font class="a"> العراق </font>  ● · نورت وطنك هنا نتعرف ع بعض تحت سماء شات العراق دردشة العراق تحشيش وضحك بين شباب وبنات وجميع نجوم العرب وشلة الحنين ومقهى ليالينا ونذكر لمة اهلنا الجميلة تجمع كل الاصدقاء عرب اكراد كلنا العراق نمازح بنوتات ونغازلهم بـ احاسيس و عشق ورومانسية ونعبر بحرية في ليالي بغداد و درر العراق ونهدي الخواطر ما بيننا واصايل الكلام تحت همس و عزف الناي وهدوء المشاعر و مسك درر العرب فوجودك معنا على قلبنا غالي وغالي كـ هيل وليل مع العاشقين و بنات العراق و شباب العراق وسماء عراق الرومانسية مع جميع صبايا العراق و عالم النبلاء · ● · اعضاء ساهموا (عبدالله,كوردية,حسين البصراوي,تورنيدو,اسكندر,انجي,دانية,علاء السامرائي,سكور,ضحكة,ايات,روز,ميوزك,علي الركابي,انكسار,اعصار,وهم,باقر,ندى,نهى,اطياف الحنان,مزاجية,مس,فلة,عراقية ستايل,عصفورة,فلسطينية,ريماس,فنرتوب,الرماس,فراشة بغداد,فهاوة,راما,مرسال,حنين,تعب,عسل,ملاك,نور,وناسة,كوردية,فوني,كهرمانة,اشجان,خليجي,نرمين,شمس,اسيادهم,صدى,نورهان,صهيب,زوزه,تاجر الاحزان,نصر,ياسمين,ذهب,اموري,ابو الذوق,ولاية بطيخ,ملك الساحة,الهيبة,ايناس,خواطر السلطان,شاهيناز,بوس الواوا,حساسة ودمعتي الماسة,قلوب,ناسيك يا زمن,فيروز,زهره اللوتس,قاهرهم,حلم,ايمان,وهاج الاصيل,كادي,بنوتة قلبي,احمد,علي,بمبي,زين,حاقد,عباس,رسول,مؤمل,كيك,حيدر,جاسم,برنسيسة,عقيل,شكو ماكو,كرار,عاصم,زينبية,سدين,برغي,جعفر,فرح,اماني,باربي,دانة,حرية,مجروحة ) · ● · مع تمنياتنا للجميع بأطيب الاوقات&nbsp;</marquee><div>`).insertBefore("div#tlogins .lonline.light.break"), $(`<a href="https://chat-iraq.com/mobile.html" target="_blank" style="width:107px;color: #E8415D;text-decoration:none; margin-right: 5px;margin-left: 2px;text-align: center;float: right;border-radius: 5px;border-color: #F2F2F2;padding: 3px;" class="border" ><i class="fa fa-android"></i>&nbsp;التطبيق</a>`).insertBefore("a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg"), $(".nav-tabs").addClass("fl").css("width", "100%");
+var _0xb650 = ["maxlength", "75", "attr", "textarea#tbox.fl.corner.tbox.filw"];
+$(_0xb650[3])[_0xb650[2]](_0xb650[0], _0xb650[1]);
+var _0xb650 = ["maxlength", "12", "attr", "div#tlogins input"];
+$(_0xb650[3])[_0xb650[2]](_0xb650[0], _0xb650[1]), $("div#tlogins button.btn.btn-primary").click(function() {
+    var c = setInterval(function() {
+        var a = $(".pmsgc").length;
+        0 < a ? ($(`<iframe src="https://mixlr.com/arabradio-sv/embed?color=1A90A5&amp;artwork=false" width="100%" height="160px" scrolling="no" frameborder="no" marginheight="0" marginwidth="0"></iframe></div><center>`).appendTo("#mic"), clearInterval(c)) : console.log(a)
+    }, 1)
+}), $(`<link rel="stylesheet" href="https://iqr30.com/IQ/stylle.css"><style>.border {
+    border: 1px solid #36b5cc;
+}
+</style>`).insertBefore("body");
+
+function logout() {
+    setTimeout(function() {
+        close(0)
+    })
+}
+$("div#tlogins button.btn.btn-primary").click(function() {
+    setTimeout(function() {
+        refreshonline()
+    }, 0)
+}), setInterval(function() {
+    var b = $("#dpnl");
+    0 < b.length && b.remove()
+}, 0), $(".dpnl").append(`   
+<div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"> 
+          <center></div><center>
+      </div>`), eval(function(p, a, c, k, e, d) {
+    e = function(c) {
+        return (c < a ? '' : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))
+    };
+    if (!''.replace(/^/, String)) {
+        while (c--) {
+            d[e(c)] = k[c] || e(c)
+        }
+        k = [function(e) {
+            return d[e]
+        }];
+        e = function() {
+            return '\\w+'
+        };
+        c = 1
+    };
+    while (c--) {
+        if (k[c]) {
+            p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c])
+        }
+    }
+    return p
+}('$(\'#C\').d(`<1 6="\\4\\e\\f \\2\\g\\5\\h\\i\\2\\j \\2\\k\\4\\2\\5"n="#"c="$(\'.p\').o($(8).r(\'6\'));s($(8),\'a\');u(v(){$(\'#3\').w(0);},0);$(\'.x\').y();"7-z="A"7-q="#3"b="B 9 1 1-a 9-m-l">&t;</1>`);', 39, 39, '|label|u0627|mic|u0634|u062A|title|data|this|fa|primary|class|onclick|append|u0631|u062D|u0633|u062E|u062F|u0645|u0644|up|volume|href|text|pnhead|target|attr|hl|nbsp|setTimeout|function|scrollTop|dpnl|show|toggle|tab|ae|d0'.split('|'), 0, {}))
